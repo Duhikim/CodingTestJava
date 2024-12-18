ca52e8fc32605bb907f6119d16114338872b78f9
Duhikim <macksa29@hotmail.com>
2024-12-18 10:47:00 +0900

    [2024.12.18] Add programmers lv2 quiz: Filling tiles 2xN

---
+++ Programmers/Tiling2xN/Solution.java
@@ -0,0 +1,16 @@
+package CodingTestStudy.Tiling2xN;
+
+class Solution {
+	public int solution(int n) {
+		long dp[] = new long[n+1];
+		long mod = 1_000_000_007;
+		// F(N) = F(N-1)*1 + F(N-2)*1 끗?
+
+		dp[0] = 1; dp[1] = 1;
+		for(int i=2; i<=n; i++){
+			dp[i] = (dp[i-1] + dp[i-2])%mod;
+		}
+
+		return (int)dp[n];
+	}
+}
