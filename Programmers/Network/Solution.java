03b9c3cf2d95df18bd9036639c015339fa152313
Duhikim <macksa29@hotmail.com>
2024-12-24 12:56:54 +0900

    [2024.12.24] Add programmers lv3 quiz: Network (not solve)

---
+++ Programmers/Network/Solution.java
@@ -0,0 +1,27 @@
+package CodingTestStudy.Network;
+
+/*
+<풀이>
+최초 네트워크 수를 1이라 놓고 시작한다.
+연결되어 있는 컴퓨터들을 모두 없애고 더이상 없앨게 없는데 남아있는 컴퓨터가 있다면 1을 추가한다.
+끝까지 반복.
+
+
+ */
+
+
+import java.util.HashMap;
+
+public class Solution {
+
+	public int solution(int n, int[][] computers) {
+		int answer = 1;
+
+		HashMap<Integer, Integer> comMap = new HashMap<>();
+
+
+
+
+		return answer;
+	}
+}
