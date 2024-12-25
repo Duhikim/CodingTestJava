4e94fc7084e3a747c8bc10b440d9260775ca4efe
Duhikim <macksa29@hotmail.com>
2024-12-25 22:42:07 +0900

    [2024.12.25] Solve programmers lv3 quiz: Network

--- Programmers/Network/Solution.java
+++ Programmers/Network/Solution.java
@@ -2,26 +2,32 @@ package CodingTestStudy.Network;
 
 /*
 <풀이>
-최초 네트워크 수를 1이라 놓고 시작한다.
 연결되어 있는 컴퓨터들을 모두 없애고 더이상 없앨게 없는데 남아있는 컴퓨터가 있다면 1을 추가한다.
 끝까지 반복.
-
-
  */
 
-
-import java.util.HashMap;
 
 public class Solution {
 
 	public int solution(int n, int[][] computers) {
-		int answer = 1;
+		int answer = 0;
 
-		HashMap<Integer, Integer> comMap = new HashMap<>();
-
-
-
-
+		boolean[] checked = new boolean[n];
+		for(int i=0; i<n; i++){
+			if(!checked[i]) {
+				search(i, computers, checked);
+				answer++;
+			}
+		}
 		return answer;
+	}
+
+	public void search(int s, int[][] computers, boolean[] checked){
+		checked[s] = true;
+		for(int i=0; i<computers.length; i++){
+			if(!checked[i] && computers[s][i]==1){
+				search(i, computers, checked);
+			}
+		}
 	}
 }
