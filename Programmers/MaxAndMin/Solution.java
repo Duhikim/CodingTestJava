7a33340f051dc8a95efddbc19282efaf58604635
Duhikim <macksa29@hotmail.com>
2024-12-26 10:58:39 +0900

    [2024.12.26] Stream example with programmers lv2 quiz: Maximum and Minimum

---
+++ Programmers/MaxAndMin/Solution.java
@@ -0,0 +1,15 @@
+package CodingTestStudy.MaxAndMin;
+
+import java.util.Arrays;
+
+public class Solution {
+	public String solution(String s) {
+		String[] str = s.split(" ");
+		int[] intArr = Arrays.stream(str)
+				.mapToInt(Integer::parseInt)
+				.toArray();
+		Arrays.sort(intArr);
+
+		return intArr[0] + " " + intArr[intArr.length-1];
+	}
+}
