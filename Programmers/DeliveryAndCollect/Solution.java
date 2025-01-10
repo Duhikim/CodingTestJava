cd2351f39889c58284d5f4facd359b1ba7d8b1a0
Duhikim <macksa29@hotmail.com>
2025-01-10 12:35:03 +0900

    [2025.1.10] Solve pg lv2 quiz: Delivery and Collect

--- Programmers/DeliveryAndCollect/Solution.java
+++ Programmers/DeliveryAndCollect/Solution.java
@@ -1,60 +1,51 @@
 package CodingTestStudy.DeliveryAndCollect;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Solution {
 	public static void main(String[] args) {
-		//4, 4, [25, 24, 51, 0], [51, 0, 0, 49]
 		Solution sol = new Solution();
-		int cap=4;
-		int n = 4;
-		int[] deliveries = {25, 24, 51, 0};
-		int[] pickups = {51, 0, 0, 49};
-		long ans = sol.solution(cap, n, deliveries, pickups);
+		int cap=1;
+		int n = 5;
+		int[] deliveries = {0, 0, 1, 0, 0};
+		int[] pickups = {0, 0, 0, 0, 0};
+		sol.solution(cap, n, deliveries, pickups);
 	}
 
 	public long solution(int cap, int n, int[] deliveries, int[] pickups) {
 		long answer = 0;
-		int longest = -1;
-		for(int i=n-1; i>=0; i--){
-			if(deliveries[i] != 0 || pickups[i] != 0) {
-				longest = i;
-				break;
+
+		ArrayList<Integer> deliveries_ = Arrays.stream(deliveries)
+				.boxed()
+				.collect(Collectors.toCollection(ArrayList::new));
+		ArrayList<Integer> pickups_ = Arrays.stream(pickups)
+				.boxed()
+				.collect(Collectors.toCollection(ArrayList::new));
+
+		while (!deliveries_.isEmpty() && deliveries_.get(deliveries_.size() - 1) == 0) {
+			deliveries_.remove(deliveries_.size() - 1);
+		}
+		while (!pickups_.isEmpty() && pickups_.get(pickups_.size() - 1) == 0) {
+			pickups_.remove(pickups_.size() - 1);
+		}
+
+		while(!deliveries_.isEmpty() || !pickups_.isEmpty()) {
+			answer += (Math.max(deliveries_.size(), pickups_.size()))*2L;
+
+			int crateDel = cap, cratePic = cap;
+
+			while(crateDel-->0 && !deliveries_.isEmpty()){
+				deliveries_.set(deliveries_.size()-1, deliveries_.get(deliveries_.size()-1)-1);
+				while(!deliveries_.isEmpty() && deliveries_.get(deliveries_.size()-1)==0) deliveries_.remove(deliveries_.size() - 1);
+			}
+			while(cratePic-->0 && !pickups_.isEmpty()){
+				pickups_.set(pickups_.size()-1, pickups_.get(pickups_.size()-1)-1);
+				while(!pickups_.isEmpty() && pickups_.get(pickups_.size()-1)==0) pickups_.remove(pickups_.size() - 1);
 			}
 		}
-		while(longest >= 0) {
-			answer += (longest+1)*2L;
-			int delCrate = cap, picCrate = cap;
-			for (int i = longest; i >= 0; i--) {
-				if (delCrate > 0 && deliveries[i] != 0) {
-					if (deliveries[i] >= delCrate) {
-						deliveries[i] -= delCrate;
-						delCrate = 0;
-					} else {
-						delCrate -= deliveries[i];
-						deliveries[i] = 0;
-					}
-				}
-				if (picCrate > 0 && pickups[i] != 0) {
-					if (pickups[i] >= picCrate) {
-						pickups[i] -= picCrate;
-						picCrate = 0;
-					} else {
-						picCrate -= pickups[i];
-						pickups[i] = 0;
-					}
-				}
-				if (picCrate == 0 && delCrate == 0) {
-					i = longest;
-					while (i >= 0 && (pickups[i] == 0 && deliveries[i] == 0)) {
-						i--;
-					}
-					longest = i;
-					break;
-				}
-				if(i==0){
-					longest = -1; break;
-				}
-			}
-		}
+
 
 		return answer;
 	}
