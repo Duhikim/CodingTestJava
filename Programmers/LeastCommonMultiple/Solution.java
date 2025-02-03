d079c09cc7d88b8ea2c66853f2641e5d4b78a228
Duhikim <macksa29@hotmail.com>
2025-02-03 12:29:58 +0900

    [2025.2.3] Solve pg lv2 quiz: Least Common Multiple

---
+++ Programmers/LeastCommonMultiple/Solution.java
@@ -0,0 +1,54 @@
+package CodingTestStudy.LeastCommonMultiple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	int[] primeNumbers = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
+
+	public int solution(int[] arr) {
+		HashMap<Integer, Integer> LCM = new HashMap<>();
+
+		for(int n: arr){
+			HashMap<Integer, Integer> primes = PrimeFactorization(n);
+			for(Map.Entry<Integer, Integer> es : primes.entrySet()) {
+				if(LCM.containsKey(es.getKey())){
+					LCM.put(es.getKey(), Math.max(es.getValue(), LCM.get(es.getKey())));
+				}
+				else{
+					LCM.put(es.getKey(), es.getValue());
+				}
+			}
+		}
+
+		int answer = 1;
+
+		for(Map.Entry<Integer, Integer> es : LCM.entrySet()){
+			answer *= (int) Math.pow(es.getKey(), es.getValue());
+		}
+
+
+		return answer;
+	}
+
+	public HashMap<Integer, Integer> PrimeFactorization(int n){
+		HashMap<Integer, Integer> primes = new HashMap<>();
+
+		for(int pn: primeNumbers){
+			if(pn > n) break;
+			int time = 1;
+			while(n%pn == 0 && n > 0){
+				primes.put(pn, time++);
+				n /= pn;
+			}
+		}
+
+		return primes;
+	}
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] arr = {1,2,3};
+		sol.solution(arr);
+	}
+}
