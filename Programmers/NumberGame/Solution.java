9d0de4b668a54a95bff658f82bf981131ceaa646
Duhikim <macksa29@hotmail.com>
2025-02-12 11:50:16 +0900

    [2025.2.12] Solve pg lv3 quiz: Number Game

---
+++ Programmers/NumberGame/Solution.java
@@ -0,0 +1,24 @@
+package CodingTestStudy.NumberGame;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+public class Solution {
+	public int solution(int[] A, int[] B) {
+		int answer = 0;
+
+		Arrays.sort(A);
+		Arrays.sort(B);
+		Queue<Integer> dequeB = Arrays.stream(B).boxed().collect(Collectors.toCollection(ArrayDeque::new));
+
+		for(int i=0; i<A.length; i++){
+			while(!dequeB.isEmpty() && A[i] >= dequeB.peek()){dequeB.poll();}
+			if(dequeB.isEmpty()) break;
+			dequeB.poll();
+			answer++;
+		}
+		return answer;
+	}
+}
