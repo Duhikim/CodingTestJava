04cb21b353a6179a1d1ab4f9cbc16e9594eb9b87
Duhikim <macksa29@hotmail.com>
2025-02-14 23:38:20 +0900

    [2025.2.14] Test HashMap methods

--- Programmers/SandBox/SandBox2.java
+++ Programmers/SandBox/SandBox2.java
@@ -1,5 +1,11 @@
 package CodingTestStudy.SandBox;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static java.text.ChoiceFormat.nextDouble;
+
 public class SandBox2 {
 	static class Parent {
 		int x, y;
@@ -26,8 +32,21 @@ public class SandBox2 {
 		}
 	}
 	public static void main(String[] args) {
-		Parent parent = new Child(3);
-		System.out.println(parent.getT());
+//		Parent parent = new Child(3);
+//		System.out.println(parent.getT());
+		Random random = new Random();
+
+		int[] arr = new int[100];
+		for(int i=0; i<100; i++){
+			arr[i] = (i* (int)(random.nextDouble()*100.0));
+		}
+		Map<Integer, Integer> myMap = new HashMap<>();
+		for(int i=0; i<100; i++){
+			myMap.put(arr[i], myMap.getOrDefault(myMap.get(arr[i]), 0)+1);
+		}
+		for(Map.Entry<Integer, Integer> es: myMap.entrySet()){
+			System.out.println(es.getKey() + " , " + es.getValue());
+		}
 	}
 
 }
