27c6e67bd55f0a42617cb45f1eeb678e0911e3fc
Duhikim <macksa29@hotmail.com>
2025-02-15 23:42:13 +0900

    [2025.2.15] Optimize solution (Solve)

--- Programmers/FindBigNumberRightBehind/Solution.java
+++ Programmers/FindBigNumberRightBehind/Solution.java
@@ -1,24 +1,35 @@
 package CodingTestStudy.FindBigNumberRightBehind;
 
+import java.util.Stack;
+
 public class Solution {
 	public int[] solution(int[] numbers) {
 		int[] answer = new int[numbers.length];
+		answer[numbers.length-1] = -1;
 		int max = numbers[numbers.length-1];
+		Stack<Integer> stack = new Stack<>();
+		stack.add(max);
 
 		for(int i=numbers.length-2; i>= 0; i--){
 			if(numbers[i] >= max){
 				answer[i] = -1;
 				max = numbers[i];
-				continue;
+				stack.clear();
+				stack.add(max);
 			}
-			for(int j=i+1; j<numbers.length; j++){
-				if(numbers[i]<numbers[j]){
-					answer[i] = numbers[j];
-					break;
+			else{
+				while(true){
+					if(numbers[i] < stack.peek()){
+						answer[i] = stack.peek();
+						stack.add(numbers[i]);
+						break;
+					}
+					else{
+						stack.pop();
+					}
 				}
 			}
 		}
-		answer[numbers.length-1] = -1;
 
 		return answer;
 	}
