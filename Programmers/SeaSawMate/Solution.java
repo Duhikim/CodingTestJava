75f69733d779e58e328ad51a915dc8fe406ab1f1
Duhikim <macksa29@hotmail.com>
2025-03-12 10:56:26 +0900

    [2025.3.12] Sea Saw Mate

---
+++ Programmers/SeaSawMate/Solution.java
@@ -0,0 +1,31 @@
+package CodingTestStudy.SeaSawMate;
+
+import java.util.*;
+
+public class Solution {
+	public long solution(int[] weights) {
+		long answer = 0;
+
+		Map<Integer, Integer> weightsMap = new HashMap<>();
+
+		for(int weight: weights){
+			weightsMap.put(weight, weightsMap.getOrDefault(weight, 0) + 1);
+		}
+
+		for(Map.Entry<Integer, Integer> es: weightsMap.entrySet()){
+			int sameWeight = es.getValue();
+			answer += (long) sameWeight * (sameWeight-1) / 2; // nC2
+			if(es.getKey() % 2 == 0 && weightsMap.containsKey(es.getKey() * 3/2)){ // 2M : 3M
+				answer += (long) es.getValue() * weightsMap.get(es.getKey() * 3/2);
+			}
+			if(weightsMap.containsKey(es.getKey() * 2)){ // 2M : 4M
+				answer += (long) es.getValue() * weightsMap.get(es.getKey() * 2);
+			}
+			if(es.getKey() % 3 == 0 && weightsMap.containsKey(es.getKey() * 4/3)){ // 3M : 4M
+				answer += (long) es.getValue() * weightsMap.get(es.getKey() * 4/3);
+			}
+		}
+
+		return answer;
+	}
+}
