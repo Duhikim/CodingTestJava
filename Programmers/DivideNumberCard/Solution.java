481d2407de7c44fd861eefcae732b16252acd157
Duhi Kim <macksa29@hotmail.com>
2025-07-24 23:48:58 +0900

    [2025.7.24] LV2 Divide Number Card

---
+++ Programmers/DivideNumberCard/Solution.java
@@ -0,0 +1,41 @@
+package CodingTestStudy.DivideNumberCard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public int solution(int[] arrayA, int[] arrayB) {
+
+        Arrays.sort(arrayA);
+        List<Integer> divisor = new ArrayList<>();
+        divisor.add(arrayA[0]);
+        // O(N), N= 100,000,000 OK
+        for(int i=2; i<=arrayA[0]/2; i++){
+            if(arrayA[0] % i != 0) continue;
+            divisor.add(i);
+            divisor.add(arrayA[0]/i);
+        }
+        // divisor의 크기는 최대로 잡아도 200이 넘지 않음
+        divisor.sort((a, b) -> b-a);
+
+        for(int i=divisor.size()-1; i>=0; i--){
+            int div = divisor.get(i);
+            for(int j=1; j<arrayA.length; j++){
+                if(arrayA[j] % div != 0){
+                    divisor.remove(i);
+                    break;
+                }
+            }
+        }
+
+        // O(200*N), N=500,000, 200*N = 100,000,000 OK
+        for(int div: divisor){
+            for(int j=0; j<arrayB.length; j++){
+                if(arrayB[j] % div == 0 ) break;
+                if(j==arrayB.length-1) return div;
+            }
+        }
+        return 0;
+    }
+}
