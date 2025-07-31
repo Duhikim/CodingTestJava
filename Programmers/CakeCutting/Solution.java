473dbe831ab25e4eba2f4b528f1c3ea2471d16b7
Duhi Kim <macksa29@hotmail.com>
2025-07-31 21:26:22 +0900

    [2025.7.31] LV2 Cake Cutting

---
+++ Programmers/CakeCutting/Solution.java
@@ -0,0 +1,28 @@
+package CodingTestStudy.CakeCutting;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+
+        Map<Integer, Integer> toppingsA = new HashMap<>();
+        Map<Integer, Integer> toppingsB = new HashMap<>();
+        for(int top: topping){
+            toppingsB.put(top, toppingsB.getOrDefault(top, 0)+1);
+        }
+
+        for(int i=0; i<topping.length-1; i++){
+            int top = topping[i];
+
+            toppingsA.put(top, toppingsA.getOrDefault(top, 0)+1);
+            toppingsB.put(top, toppingsB.get(top)-1);
+            if(toppingsB.get(top)==0) toppingsB.remove(top);
+
+            if(toppingsA.size() == toppingsB.size()) answer++;
+            else if(toppingsA.size() > toppingsB.size()) break;
+        }
+
+        return answer;
+    }
+}
